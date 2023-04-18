73110a8e89dcf85412867115d624d66370380ef5
dhairyagupta2603 <dhairyagupta2603@gmail.com>
2023-04-18 20:37:05 +0530

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhairya/projects/ros/autonomous_DSLAM/src
+# Build directory: /home/dhairya/projects/ros/autonomous_DSLAM/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/dhairya/projects/ros/autonomous_DSLAM/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/dhairya/cse3102/catkin_ws3/devel;/opt/ros/noetic'.split(';') if '/home/dhairya/cse3102/catkin_ws3/devel;/opt/ros/noetic' != '' else []
